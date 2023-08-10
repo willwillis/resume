d61f9568cd4dd14d1b8dd00e27134ae550f95133
Will Willis <will.willis@gmail.com>
2023-08-10 16:25:03 -0500

    initial commit

---
+++ config/site.ts
@@ -0,0 +1,16 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Will Willis",
+  description:
+    "Will's Resume",
+  mainNav: [
+    {
+      title: "Resume",
+      href: "/",
+    },
+  ],
+  links: {
+    github: "https://github.com/willwillis",
+  },
+}
