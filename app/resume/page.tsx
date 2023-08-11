801b8aed7ad474d0cf0d6d4e073199a8788be969
Will Willis <will.willis@gmail.com>
2023-08-11 17:45:42 -0500

    amplify can't handle single quotes?

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -8,7 +8,7 @@ export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-    <h1>This can't be how to introduce new colors, right? 3 files?</h1>
+    <h1>This can&apos;t be how to introduce new colors, right? 3 files?</h1>
     <Badge variant={"default"}>default badge</Badge>
     <Badge variant={"secondary"}>secondary badge</Badge>
     <Badge variant={"destructive"}>destructive badge</Badge>
